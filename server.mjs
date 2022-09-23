6ce99e7c329daad0f6f6d658c63f349be3b59e0d
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-09-23 20:11:10 +0500

    todo app with server

--- server.mjs
+++ server.mjs
@@ -1,14 +1,30 @@
 console.log("I am Hiten Kumar");
 
 import express from 'express';
+import cors from 'cors'
 const app = express()
-const port = process.env.PORT||3000;
+const port = process.env.PORT||4000;
 
-app.get('/', (req, res) => {
-    console.log("someone is requesting on this server", new Date());
-    console.log("some changes")
-  res.send('Hello World form Hiten Kumar!')
+let todos = [];
+
+app.use(express.json());
+app.use(cors())
+app.post('/todo',(req,res)=>{
+  
+  todos.push(req.body.text);
+req.send({
+
+  message:"Your todo is saved",
+  data:todos
 })
+
+})
+app.get('/todos', (req, res) => {
+
+  res.send({
+    message:"Here is todos list "
+    data: todos
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
